687fd4dbdca679c17ed45640927b818946e9200e
ekayaint <ekayaint@gmail.com>
2023-09-23 15:07:46 +0300

    Initial Commit

---
+++ SwiftUI-WeatherApp-Sample/SwiftUI_WeatherApp_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp_SampleApp.swift
+//  SwiftUI-WeatherApp-Sample
+//
+//  Created by ekayaint on 23.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
