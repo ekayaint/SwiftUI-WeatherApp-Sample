87bf99875e7247f75cdc1645088a856771c417ec
ekayaint <130402472+ekayaint@users.noreply.github.com>
2023-09-26 06:45:58 +0300

    Update WeatherManager.swift

--- SwiftUI-WeatherApp-Sample/Managers/WeatherManager.swift
+++ SwiftUI-WeatherApp-Sample/Managers/WeatherManager.swift
@@ -9,7 +9,7 @@ import Foundation
 import CoreLocation
 
 class WeatherManager {
-    private let apiKey = "1bb5c3a53f10a7df67f6d887ad422fb5"
+    private let apiKey = "###"
     
     func getWeatherData(for coordinate: CLLocationCoordinate2D, completion: @escaping (Result<WeatherResponse, WeatherError>) -> Void) {
         guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(coordinate.latitude)&lon=\(coordinate.longitude)&appid=\(apiKey)&units=metric") else {
